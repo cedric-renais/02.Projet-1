d5e53ed6d03c86b8193d61abacb5a69caaa58058
Cédric RENAIS <cedric.renais@gmail.com>
2024-07-20 15:28:02 +0200

    🚧 Ajout gestions des display & des erreurs 🐛 Correction dans le composant ListItem 💄 Déplacement bg-color du container vers le body

--- src/App.jsx
+++ src/App.jsx
@@ -3,44 +3,60 @@ import { useState } from 'react';
 import ListItem from './components/ListItem';
 
 function App() {
-  const [todoList, SetTodoList] = useState([
-    {
-      id: nanoid(8),
-      item: 'item 1',
-    },
-    {
-      id: nanoid(8),
-      item: 'item 2',
-    },
-    {
-      id: nanoid(8),
-      item: 'item 3',
-    },
-  ]);
+  const [todoList, setTodoList] = useState([]);
+
+  const [todo, setTodo] = useState('');
+  const [showValidation, setShowValidation] = useState(false);
 
   function deleteTodo(id) {
-    SetTodoList(todoList.filter((item) => item.id !== id));
+    setTodoList(todoList.filter((todo) => todo.id !== id));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (todo.trim() === '') {
+      setShowValidation(true);
+      return;
+    }
+    if (todo.trim() !== '') {
+      setTodoList([...todoList, { id: nanoid(8), content: todo }]);
+      setTodo('');
+      setShowValidation(false);
+    }
   }
 
   return (
-    <div className="h-screen bg-[#13131E]">
+    <div className="h-screen">
       <div className="max-w-4xl mx-auto pt-20 px-6">
         <h1 className="text-3xl text-[#B1A9FF] mb-4">Ma To-do liste</h1>
-        <form className="mb-10">
+        <form onSubmit={handleSubmit} className="mb-10">
           <label htmlFor="todo-item" className="text-[#E0DFFE]">
             Ajouter une chose à faire
           </label>
           <input
             type="text"
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
             className="mt-1 block w-full bg-[#E0DFFE] text-[#13131E] rounded focus:ring-[#3D3E82]"
           />
+          {showValidation && (
+            <p className="text-[#E54666]">Veuillez entrer une tâche</p>
+          )}
           <button className="mt-4 mb-4 py-2 px-2 bg-[#5B5BD6] rounded min-w-[115px] hover:bg-[#6E6ADE]">
             Ajouter
           </button>
           <ul>
-            {todoList.map((item) => (
-              <ListItem key={item.id} itemData={item} deleteTodo={deleteTodo} />
-            ))}
+            {todoList.length === 0 && (
+              <li className="text-[#E0DFFE]">Aucune tâche à faire...</li>
+            )}
+            {todoList.length > 0 &&
+              todoList.map((item) => (
+                <ListItem
+                  key={item.id}
+                  itemData={item}
+                  deleteTodo={deleteTodo}
+                />
+              ))}
           </ul>
         </form>
       </div>
--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,7 +1,7 @@
 export default function ListItem({ itemData, deleteTodo }) {
   return (
     <li className="p-2 mt-2 bg-[#E0DFFE] text-[#13131E] mb-2 rounded flex">
-      <span>{itemData.item}</span>
+      <span>{itemData.content}</span>
       <button
         onClick={() => deleteTodo(itemData.id)}
         className="ml-auto py-1 px-2 font-semibold bg-[#E54666] text-[#E0DFFE] rounded"
