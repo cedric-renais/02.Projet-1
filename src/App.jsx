4b87b671efaf24cab4743203514c1675291daf39
Cédric RENAIS <cedric.renais@gmail.com>
2024-07-20 14:49:23 +0200

    🚧 Création & Suppression des élements ➕ Installation de nonaid pour la gestion des ids 🚀 Ajout du composant ListItem

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,27 @@
+import { nanoid } from 'nanoid';
+import { useState } from 'react';
+import ListItem from './components/ListItem';
+
 function App() {
+  const [todoList, SetTodoList] = useState([
+    {
+      id: nanoid(8),
+      item: 'item 1',
+    },
+    {
+      id: nanoid(8),
+      item: 'item 2',
+    },
+    {
+      id: nanoid(8),
+      item: 'item 3',
+    },
+  ]);
+
+  function deleteTodo(id) {
+    SetTodoList(todoList.filter((item) => item.id !== id));
+  }
+
   return (
     <div className="h-screen bg-[#13131E]">
       <div className="max-w-4xl mx-auto pt-20 px-6">
@@ -11,9 +34,14 @@ function App() {
             type="text"
             className="mt-1 block w-full bg-[#E0DFFE] text-[#13131E] rounded focus:ring-[#3D3E82]"
           />
-          <button className="mt-4 py-2 px-2 bg-[#5B5BD6] rounded min-w-[115px] hover:bg-[#6E6ADE]">
+          <button className="mt-4 mb-4 py-2 px-2 bg-[#5B5BD6] rounded min-w-[115px] hover:bg-[#6E6ADE]">
             Ajouter
           </button>
+          <ul>
+            {todoList.map((item) => (
+              <ListItem key={item.id} itemData={item} deleteTodo={deleteTodo} />
+            ))}
+          </ul>
         </form>
       </div>
     </div>
---
+++ src/components/ListItem.jsx
@@ -0,0 +1,13 @@
+export default function ListItem({ itemData, deleteTodo }) {
+  return (
+    <li className="p-2 mt-2 bg-[#E0DFFE] text-[#13131E] mb-2 rounded flex">
+      <span>{itemData.item}</span>
+      <button
+        onClick={() => deleteTodo(itemData.id)}
+        className="ml-auto py-1 px-2 font-semibold bg-[#E54666] text-[#E0DFFE] rounded"
+      >
+        X
+      </button>
+    </li>
+  );
+}
